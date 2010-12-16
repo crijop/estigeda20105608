781055e0a6d5238134f7cb00cf8926ea5cf73635
crijop@gmail.com <crijop@gmail.com@2a9c9612-fd90-d559-4d99-dc7df1cf6e91>
2010-12-16 17:32:00 +0000

    iniciq

---
+++ trunk/src/Main.java
@@ -0,0 +1,17 @@
+package eda;
+
+public class Main
+{
+   /**
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+
+      OperacoesImagem op = new OperacoesImagem();
+      
+      op.open("images/alicates.jpg");
+      op.binarization(200);
+      op.save("images/alicatesCizento.jpg");
+   }
+}
---
+++ trunk/src/OperacoesImagem.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package eda;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+/**
+ * @author Carlos
+ * 
+ */
+public class OperacoesImagem
+{
+   private BufferedImage bi;
+   public WritableRaster wr;
+
+   public OperacoesImagem()
+   {
+      this.bi = null;
+      this.wr = null;
+   }
+
+   public void open(String nome_ficheiro)
+   {
+      File ficheiro;
+      ficheiro = new File(nome_ficheiro);
+      try
+      {
+         this.bi = ImageIO.read(ficheiro);
+      } catch (IOException ex)
+      {
+         Logger.getLogger(OperacoesImagem.class.getName()).log(Level.SEVERE,
+                  null, ex);
+      }
+      this.wr = this.bi.getRaster();
+   }
+
+   public void save(String nome_ficheiro)
+   {
+      File ficheiro;
+      ficheiro = new File(nome_ficheiro);
+      try
+      {
+         ImageIO.write(this.bi, "jpg", ficheiro);
+      } catch (IOException ex)
+      {
+         Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+
+   public int getWidth()
+   {
+      return bi.getWidth();
+   }
+
+   public int getHeight()
+   {
+      return bi.getHeight();
+   }
+
+   public void binarization(int th)
+   {
+      int red, green, blue;
+      int coluna, linha;
+      int nivel_cinzento;
+
+      for (coluna = 0; coluna < this.getWidth(); coluna++)
+      {
+         for (linha = 0; linha < this.getHeight(); linha++)
+         {
+            int[] color = null;
+            color = this.wr.getPixel(coluna, linha, color);
+
+            red = color[0];
+            green = color[1];
+            blue = color[2];
+
+            nivel_cinzento = (int) ((red + green + blue) / 3.0);
+
+            if (nivel_cinzento < th)
+            {
+               color[0] = 0xff;
+               color[1] = 0xff;
+               color[2] = 0xff;
+               this.wr.setPixel(coluna, linha, color);
+            }
+            else
+            {
+               color[0] = 0x00;
+               color[1] = 0x00;
+               color[2] = 0x00;
+               this.wr.setPixel(coluna, linha, color);
+            }
+         }
+
+      }
+   }
+}
